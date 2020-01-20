f6fe94fcb33665f2870df396e51e617eff6b88d1
Rafael Pazos <rarod@microsoft.com>
2020-01-20 12:56:23 +0100

    fix math

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -24,12 +24,7 @@ namespace simpleaspnetcore.Controllers
             return "value";
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
+       
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
